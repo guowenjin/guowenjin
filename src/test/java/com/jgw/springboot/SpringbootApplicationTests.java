53774153d59106dfce8e23b8d1db0233e6d0917b
63952DDCACAD23AC76BA8330CFF138BD <63952DDCACAD23AC76BA8330CFF138BD@i-search.com.cn>
2020-04-03 14:33:22 +0800

    springboot框架

---
+++ src/test/java/com/jgw/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jgw.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
